f1eb636b2f8fd6d7909fe1f53260a116709e2966
Nisarg <nisarg.panchal@ucareqld.com.au>
2023-07-15 16:02:27 +1000

    Initial commit:dockerized

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO person (id, name, email, age, gender) VALUES (1, 'John Smith', 'john.smith@test.com', 30, 'Male');
-INSERT INTO person (id, name, email, age, gender) VALUES (2, 'Eva Greene', 'eva@test.com', 31, 'Female');
-INSERT INTO person (id, name, email, age, gender) VALUES (3, 'Angela Smith', 'angel@test.com', 32, 'Female');
-INSERT INTO person (id, name, email, age, gender) VALUES (4, 'Claudia Goldsmith', 'claudia@test.com', 35, 'Female');
+INSERT INTO people (id, name, email, age, gender) VALUES (1, 'John Smith', 'john.smith@test.com', 30, 'Male');
+INSERT INTO people (id, name, email, age, gender) VALUES (2, 'Eva Greene', 'eva@test.com', 31, 'Female');
+INSERT INTO people (id, name, email, age, gender) VALUES (3, 'Angela Smith', 'angel@test.com', 32, 'Female');
+INSERT INTO people (id, name, email, age, gender) VALUES (4, 'Claudia Goldsmith', 'claudia@test.com', 35, 'Female');
