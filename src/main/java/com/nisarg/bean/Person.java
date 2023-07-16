58a55ef5387e28232d5037202599e784cddbe160
Nisarg <nisarg.panchal@ucareqld.com.au>
2023-07-16 12:59:39 +1000

    Initial commit:

--- src/main/java/com/nisarg/bean/Person.java
+++ src/main/java/com/nisarg/bean/Person.java
@@ -1,5 +1,7 @@
 package com.nisarg.bean;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,12 +26,17 @@ public class Person {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Schema(name = "ID of a person", description = "System generated and unknown to person", requiredMode = RequiredMode.REQUIRED)
   long id;
   @NotBlank
+  @Schema(name = "Full Name of a person", description = "Full Name", requiredMode = RequiredMode.REQUIRED)
   String name;
   @Email
+  @Schema(name = "Email of a person", description = "Must be a valid email address", requiredMode = RequiredMode.REQUIRED)
   String email;
   @Min(1)
+  @Schema(name = "Person's age", description = "Age must be 18 or above, no minors", requiredMode = RequiredMode.REQUIRED)
   int age;
+  @Schema(name = "Person's gender", description = "Refers to birth-time biological gender (Male/Female)", requiredMode = RequiredMode.REQUIRED)
   String gender;
 }
